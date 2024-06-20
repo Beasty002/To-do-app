d722bedff10464744466611e3fe6e93a164ede49
Beasty002 <xasbin12@gmail.com>
2024-06-20 09:56:58 +0545

    add dragging feature

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const inputBox = document.getElementById("inputBox");
 const listContainer = document.getElementById("listContainer");
 const btn = document.querySelector("button");
-btn.addEventListener("click", addtask);
-function addtask() {
+btn.addEventListener("click", addTask);
+
+function addTask() {
     if (inputBox.value === '') {
         alert("Write something first");
-    }
-    else {
+    } else {
         let li = document.createElement("li");
+        li.classList.add("draggable");
+        li.setAttribute("draggable", "true");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         inputBox.value = "";
@@ -15,27 +17,60 @@ function addtask() {
         span.innerHTML = "\u00d7";
         li.appendChild(span);
         saveData();
+        addDragEvents(li);
     }
 }
-
 
 listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") { //always in uppercase
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
-
-    }
-    else if (e.target.tagName === "SPAN") {
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
-
     }
-}, false)
+});
 
 function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML)
+    localStorage.setItem("data", listContainer.innerHTML);
 }
+
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
+    const items = listContainer.querySelectorAll("li");
+    items.forEach(item => addDragEvents(item));
 }
 showTask();
+
+function addDragEvents(draggable) {
+    draggable.addEventListener('dragstart', () => {
+        draggable.classList.add('dragging');
+    });
+    draggable.addEventListener('dragend', () => {
+        draggable.classList.remove('dragging');
+    });
+}
+
+listContainer.addEventListener('dragover', e => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(listContainer, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    if (afterElement === null) {
+        listContainer.appendChild(draggable);
+    } else {
+        listContainer.insertBefore(draggable, afterElement);
+    }
+});
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child };
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
